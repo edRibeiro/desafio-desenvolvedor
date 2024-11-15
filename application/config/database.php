1033f3a441c09470be67be8b213538f339dec78a
edRibeiro <edersonribeiro.ita@gmail.com>
2024-11-14 21:21:12 -0300

    conf: Configurando o MongoDB na aplicação.

--- application/config/database.php
+++ application/config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -112,9 +112,15 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
         'mongodb' => [
-            'driver' => 'mongodb',
-            'dsn' => env('MONGODB_URI', 'mongodb://localhost:27017'),
-            'database' => env('MONGODB_DATABASE', 'laravel_app'),
+            'driver'   => 'mongodb',
+            'host'     => env('DB_HOST'),
+            'port'     => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options'  => [
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin') // required with Mongo 3+
+            ]
         ],
 
     ],
